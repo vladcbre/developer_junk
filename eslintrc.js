d1fe4e80f5c2f6d6089adbcd5ebe1c699fa57b3c
Vlad <32068524+vladcbre@users.noreply.github.com>
2017-10-05 15:15:55 -0400

    + initial eslintrc.js

---
+++ eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "env": {
+        "browser": true
+    },
+    "rules": {
+        "keyword-spacing": [
+            "error",
+            { after: true, before: true }
+        ],
+        "indent": [
+            "error",
+            2
+        ],
+        "eqeqeq": [
+            "error"
+        ],
+        "object-curly-spacing": [
+            "error", "always"
+        ],
+        "curly": [
+            "error", "multi-line"
+        ]
+    }
+}
