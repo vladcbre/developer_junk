a9a9aaeac0bd3b685315a56a0f043ee047d92384
Vlad <32068524+vladcbre@users.noreply.github.com>
2017-10-05 16:55:18 -0400

    adding quotes from style guide

--- eslintrc.js
+++ eslintrc.js
@@ -1,27 +1,46 @@
+// quotes from https://github.com/floored/flbin/wiki/Standards:-Javascript
 module.exports = {
-    "env": {
-        "browser": true
-    },
-    "rules": {
-        "keyword-spacing": [
-            "error",
-            { after: true, before: true }
-        ],
-        "indent": [
-            "error",
-            2
-        ],
-        "eqeqeq": [
-            "error"
-        ],
-        "object-curly-spacing": [
-            "error", "always"
-        ],
-        "curly": [
-            "error", "multi-line"
-        ],
-        "semi": [
-            "error", "always"
-        ]
-    }
+  "env": {
+    "browser": true
+  },
+  "rules": {
+    // * Whitespace
+    // Use soft tabs set to 2 spaces.
+    "indent": [
+      "error",
+      2
+    ],
+    // Always put spaces around binary operators
+    "space-infix-ops": [
+      "error"
+    ],
+    // Always put a space between if, for, etc. and the following (, or everyone will shun you.
+    "keyword-spacing": [
+      "error",
+      { after: true, before: true }
+    ],
+    "object-curly-spacing": [
+      "error", "always"
+    ],
+
+    // * Blocks
+    // Braces are required for all multi-line blocks.
+      "curly": [
+      "error", "multi-line"
+    ],
+    //No extra line returns; it wastes vertical space and can interact poorly with JavaScript's semicolon insertion.
+      "brace-style": [
+      "error"
+    ],
+
+    // * Equality & Type Checking
+    // Use === and !== instead of == and !=
+      "eqeqeq": [
+      "error"
+    ],
+    // * Semicolons: Use them
+      "semi": [
+      "error", "always"
+    ]
+  }
 }
